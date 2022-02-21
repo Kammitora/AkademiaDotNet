be252e1c729e10d806714ab0b262ead926b73c5c
Kamil Kowalski <kammitora@outlook.com>
2022-02-21 23:15:00 +0100

    Added two homeworks from week 4th

---
+++ T4L11PracaDomowa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4L11PracaDomowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane, spróbuj jeszcze raz");
+                    continue;
+                }
+
+                Console.WriteLine(EvenOrOdd(number));
+                Console.WriteLine("Aby zakończyć, wciśnij ESC.");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+        private static string EvenOrOdd(int number)
+        {
+            return number % 2 == 0 ? "Liczba parzysta" : "Liczba nieparzysta";
+        }
+    }
+}
---
+++ T4L13PracaDomowa2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4L13PracaDomowa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane, spróbuj jeszcze raz.");
+                }
+
+                Console.WriteLine(Game(number));
+                Console.WriteLine("Jeśli chcesz wyjść z gry, naciśnij ESC.");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+        private static string Game(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            return number.ToString();
+        }
+    }
+}
